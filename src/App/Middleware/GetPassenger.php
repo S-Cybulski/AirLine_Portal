333bf5601228e2e7944352accff5b3ed37a65edb
S-Cybulski <Szymcyb04@protonmail.com>
2024-05-16 16:21:12 +0100

    added request middleware for get passenger

---
+++ src/App/Middleware/GetPassenger.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types= 1);
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Routing\RouteContext;
+use App\Repositories\AirlineRepository;
+use Slim\Exception\HttpNotFoundException;
+
+class GetPassenger
+{
+    public function __construct(private AirlineRepository $repository)
+    {
+
+    }
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        $context = RouteContext::fromRequest($request);
+
+        $route = $context->getRoute();
+
+        $id = $route->getArgument("id");
+
+        $data = $this->repository->getPassengerById((int) $id);
+    
+        if($data === false)
+        {
+            throw new HttpNotFoundException($request, message: 'Passenger not found');
+        }
+
+        $request = $request->withAttribute('passenger', $data);
+
+        return $handler->handle($request);
+    }
+}
